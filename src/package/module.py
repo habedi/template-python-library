4c613ab07b1641449500f84387941962067ea28d
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-01-25 08:38:16 +0100

    Second iteration
    - Added a workflow file for linting.
    - Added a Makefile for managing common development tasks.
    - Updated workflow files to use the Makefile.
    - Updated badges in README.md: removed Ruff badge, added Codecov badge. Also simplified README.md and added information on using the Makefile.
    - Polished `pyproject.toml`.

--- src/package/module.py
+++ src/package/module.py
@@ -3,15 +3,15 @@
 
 class DummyClass:
     def __init__(self):
-        print('DummyClass.__init__')
+        print("DummyClass.__init__")
 
     def dummy_method(self):
-        print('DummyClass.dummy_method')
+        print("DummyClass.dummy_method")
 
     @staticmethod
     def static_method():
-        print('DummyClass.static_method')
+        print("DummyClass.static_method")
 
     @classmethod
     def class_method(cls):
-        print('DummyClass.class_method')
+        print("DummyClass.class_method")
