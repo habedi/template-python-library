5765ac1e4ff43ff6e79e69f5f774ac8a2c49f879
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-05-29 19:10:14 +0200

    Fix library versioning info

--- library/__init__.py
+++ library/__init__.py
@@ -1,12 +1,13 @@
-import importlib
 import logging
+from importlib.metadata import version, PackageNotFoundError
 
 from package import DummyClass
 
 _logger = logging.getLogger(__name__)
+
 try:
-    __version__ = importlib.metadata.version("template-python-library")
-except importlib.metadata.PackageNotFoundError:
+    __version__ = version("template-python-library")
+except PackageNotFoundError:
     __version__ = "0.0.0-unknown"
     _logger.warning(
         "Could not determine package version using importlib.metadata. "
